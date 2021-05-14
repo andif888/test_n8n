ae3348121f8e4aaabf589097c61c32c424e6d4db
andif888 <andif888@users.noreply.github.com>
2021-05-15 00:04:43 +0200

    Create download_deviceTRUST.sh

---
+++ download_deviceTRUST.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -uex
+
+devicetrust_version="20.2.200"
+
+if [ -d "/tmp/deviceTRUST" ]; then
+  rm -rf /tmp/deviceTRUST
+fi
+
+if [ -f "/tmp/deviceTRUST.zip" ]; then
+  rm -f /tmp/deviceTRUST.zip
+fi
+
+wget --progress=bar:force -O /tmp/deviceTRUST.zip 'https://storage.devicetrust.com/download/deviceTRUST-$devicetrust_version.zip'
+
+unzip /tmp/deviceTRUST.zip -d /tmp/deviceTRUST/
